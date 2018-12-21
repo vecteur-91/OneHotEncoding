73afe596f28c61bb2aea14057103a9a8f3b1899b
vecteur-91 <vecteur-91@gmail.com>
2018-12-21 17:23:27 +0100

    Init

---
+++ DataOneHot.py
@@ -0,0 +1,24 @@
+
+"""
+Random Forest OneHotEncoding
+
+"""
+
+import os
+os.getcwd()
+os.chdir("C:/Users/SIVA/Desktop/RandomForest/OneHotEncoding")
+
+
+"""
+Packages loading
+
+"""
+import numpy as np
+import pandas as pd
+
+"""
+Data Processing
+
+"""
+
+ tf
